def39f6aa379299c1d46b0785c1bd0628bdc2a39
Tonje Albertin Kristiansen <149260628+T0nj3@users.noreply.github.com>
2025-04-09 13:52:32 +0200

    add custom Tailwind config with project colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,39 @@
 module.exports = {
   content: ["./**/*.{html,js}","!./node_modules/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: {
+          base: "#252625",
+          inverted: "#FFFFFF",
+        },
+        background: {
+          primary: "#8C6C45",
+          secondary: "#DBB391",
+          light: "#FFFFFF",
+        },
+        accent: {
+          olive: "#AAA668",
+          sky: "#A0DBF8",
+        },
+        brand: {
+          blue: "#A0DBF8",
+        },
+      },
+      fontFamily: {
+        heading: ['Playfair Display', 'serif'],
+        body: ['Inter', 'sans-serif'],
+      },
+      fontSize: {
+        h1: ['2.25rem', '2.5rem'], // 36px
+        h2: ['1.875rem', '2.25rem'], // 30px
+        h3: ['1.5rem', '2rem'], // 24px
+        textLg: ['1.25rem', '1.75rem'], // 20px
+        textBase: ['1rem', '1.5rem'], // 16px
+        textSm: ['0.875rem', '1.25rem'], // 14px
+      },
+    },
   },
   plugins: [],
-}
+};
 
