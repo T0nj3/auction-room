eea5a209e9daa46c39681cffc29be0d9d0dc74a4
Andlie94 <andlie02174@stud.noroff.no>
2025-04-24 15:16:24 +0200

    Satt opp fetch til profilsiden

---
+++ js/profile-js/apiprofile.js
@@ -0,0 +1,35 @@
+export const X_NOROFF_API_KEY = "580b33a9-04f3-4da3-bb38-de9adcf9d9f8";
+export const API_BASE_URL = "https://v2.api.noroff.dev/auction"; 
+
+export async function getUserProfile() {
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+        console.error("No access; the user is not logged in.");
+        return null;
+    }
+
+    const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+        "X-Noroff-API-Key": X_NOROFF_API_KEY,
+    };
+
+    try {
+        
+        const response = await fetch(`${API_BASE_URL}/profiles/${username}`);
+
+        if (!response.ok) {
+            console.error(`Something went wrong while fetching your profile: ${response.status}`);
+            const responseText = await response.text();
+            console.error("Feil respons:", responseText);
+            return null;
+        }
+
+        const data = await response.json();
+        return data.data;
+    } catch (error) {
+        console.error("Something went wrong while fetching your profile:", error);
+        return null;
+    }
+}
