4f4c2dc72f3c2fd0d0cd5460a991dc1a85862877
Tonje Albertin Kristiansen <149260628+T0nj3@users.noreply.github.com>
2025-05-09 22:10:57 +0200

    redirecting logged in user when trying to register

--- js/auth_js/handel/registration.js
+++ js/auth_js/handel/registration.js
@@ -58,10 +58,13 @@ registerButton.addEventListener("click", async (event) => {
       // Suksess!
       showSuccess("Registration successful. Redirecting...");
     } catch (error) {
-      ShowError(error.message); // viser error med styling
+      ShowError(error.message); 
       console.error("Registrering feilet:", error.message);
     }
   }
 });
 
-localStorage.removeItem("token");
+const token = localStorage.getItem("token");
+if (token) {
+  window.location.href = "../index.html"; 
+}
